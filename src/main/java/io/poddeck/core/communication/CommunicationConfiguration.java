1ec0a213d16273a75145a6b4cbb8accc68d8776c
Lukas <contact@lukasbreuer.de>
2025-11-12 15:33:57 +0100

    Implement CommunicationConfiguration

---
+++ src/main/java/io/poddeck/core/communication/CommunicationConfiguration.java
@@ -0,0 +1,19 @@
+package io.poddeck.core.communication;
+
+import io.poddeck.common.configuration.Configuration;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
+import org.apache.commons.configuration2.AbstractConfiguration;
+
+@Getter
+@Accessors(fluent = true)
+@RequiredArgsConstructor(staticName = "create")
+public class CommunicationConfiguration implements Configuration {
+  private int port;
+
+  @Override
+  public void load(AbstractConfiguration file) {
+    port = file.getInt("communication.port");
+  }
+}
