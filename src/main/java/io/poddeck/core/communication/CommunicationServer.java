e2c41017620c36cf90dd69e6bd33b16354ddca3d
Lukas <contact@lukasbreuer.de>
2025-11-12 15:34:22 +0100

    Implement CommunicationServer

---
+++ src/main/java/io/poddeck/core/communication/CommunicationServer.java
@@ -0,0 +1,38 @@
+package io.poddeck.core.communication;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.poddeck.common.log.Log;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@Singleton
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE, onConstructor = @__({@Inject}))
+public final class CommunicationServer {
+  private final Log log;
+  private final CommunicationConfiguration configuration;
+  private Server server;
+
+  public void startAsync() {
+    new Thread(this::start).start();
+  }
+
+  public void start() {
+    try {
+      server = ServerBuilder.forPort(configuration.port())
+        .addService(HandshakeService.create()).build().start();
+      server.awaitTermination();
+    } catch (Exception exception) {
+      log.processError(exception);
+    }
+  }
+
+  public void close() throws Exception {
+    if (server == null) {
+      throw new Exception("Server never started");
+    }
+    server.shutdownNow();
+  }
+}
