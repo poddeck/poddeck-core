a3426372bdb99ae7941a389d066c1808ddd23ce8
Lukas <contact@lukasbreuer.de>
2025-11-13 13:08:41 +0100

    Implement UserAgent

---
+++ src/main/java/io/poddeck/core/user/session/UserAgent.java
@@ -0,0 +1,24 @@
+package io.poddeck.core.user.session;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(staticName = "create")
+public final class UserAgent {
+  private final String content;
+
+  public String findPlatform() {
+    var processed = content.toLowerCase();
+    if (processed.contains("windows")) {
+      return "WINDOWS";
+    } else if (processed.contains("x11")) {
+      return "LINUX";
+    } else if (processed.contains("iphone") || processed.contains("ipad")) {
+      return "IOS";
+    } else if (processed.contains("mac")) {
+      return "MAC";
+    } else if (processed.contains("android")) {
+      return "ANDROID";
+    }
+    return "";
+  }
+}
