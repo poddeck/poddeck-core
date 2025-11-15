0ab845409d826bcb7fdae209e65964fe256c5ad5
Lukas <contact@lukasbreuer.de>
2025-11-15 10:24:50 +0100

    Implement Endpoint & EndpointRepository

---
+++ src/main/java/io/poddeck/core/access/security/Endpoint.java
@@ -0,0 +1,16 @@
+package io.poddeck.core.access.security;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.lang.annotation.Annotation;
+import java.util.List;
+
+@Getter
+@Accessors(fluent = true)
+@RequiredArgsConstructor(staticName = "create")
+public final class Endpoint {
+  private final String path;
+  private final List<Class<? extends Annotation>> annotations;
+}
---
+++ src/main/java/io/poddeck/core/access/security/EndpointRepository.java
@@ -0,0 +1,46 @@
+package io.poddeck.core.access.security;
+
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.lang.annotation.Annotation;
+import java.util.List;
+
+@Component
+public final class EndpointRepository {
+  @Autowired
+  private RequestMappingHandlerMapping requestMappingHandlerMapping;
+  private final List<Endpoint> endpoints = Lists.newArrayList();
+
+  private void load() {
+    for (var entry : requestMappingHandlerMapping.getHandlerMethods().entrySet()) {
+      var info = entry.getKey();
+      var path = "/v1" + info.getPatternValues().iterator().next();
+      var annotations = Lists.<Class<? extends Annotation>>newArrayList();
+      for (var annotation : entry.getValue().getMethod().getAnnotations()) {
+        annotations.add(annotation.annotationType());
+      }
+      endpoints.add(Endpoint.create(path, annotations));
+    }
+  }
+
+  public List<Endpoint> findAnnotatedEndpoints(
+    Class<? extends Annotation> annotation
+  ) {
+    if (endpoints.isEmpty()) {
+      load();
+    }
+    return endpoints.stream()
+      .filter(endpoint -> endpoint.annotations().contains(annotation))
+      .toList();
+  }
+
+  public List<Endpoint> all() {
+    if (endpoints.isEmpty()) {
+      load();
+    }
+    return List.copyOf(endpoints);
+  }
+}
