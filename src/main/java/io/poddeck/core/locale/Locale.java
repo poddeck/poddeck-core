6a02208cbc229d3ce1d02b6b4e81dad4f7e4b9c7
Lukas <contact@lukasbreuer.de>
2025-11-15 11:01:00 +0100

    Implement Locale

---
+++ src/main/java/io/poddeck/core/locale/Locale.java
@@ -0,0 +1,57 @@
+package io.poddeck.core.locale;
+
+import com.google.common.collect.Maps;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
+import org.springframework.core.io.ClassPathResource;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+@Accessors(fluent = true)
+@RequiredArgsConstructor(staticName = "create")
+public final class Locale {
+  public static Locale createAndLoad(String language) throws Exception {
+    var configuration = create(language);
+    configuration.load();
+    return configuration;
+  }
+
+  @Getter
+  private final String language;
+  private final Map<String, String> locale = Maps.newHashMap();
+
+  private static final String LOCALE_PATH = "/locale/%s.json";
+
+  public void load() throws Exception {
+    var resource = new ClassPathResource(String.format(LOCALE_PATH, language));
+    try (var inputStream = resource.getInputStream()) {
+      var json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+      deserialize(JsonParser.parseString(json).getAsJsonObject());
+    }
+  }
+
+  private void deserialize(JsonObject json) {
+    for (var key : json.keySet()) {
+      locale.put(key, json.get(key).getAsString());
+    }
+  }
+
+  public void addLocale(Locale newLocale) {
+    locale.putAll(newLocale.locale);
+  }
+
+  public void removeLocale(Locale otherLocale) {
+    locale.keySet().removeAll(otherLocale.locale.keySet());
+  }
+
+  public String findText(String key) {
+    if (!locale.containsKey(key)) {
+      return key;
+    }
+    return locale.get(key);
+  }
+}
