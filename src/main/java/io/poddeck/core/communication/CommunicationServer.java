3ca301c2875664ac0579886b17acbeea2135ef00
Lukas <contact@lukasbreuer.de>
2025-11-22 19:26:36 +0100

    Update CommunicationServer

--- src/main/java/io/poddeck/core/communication/CommunicationServer.java
+++ src/main/java/io/poddeck/core/communication/CommunicationServer.java
@@ -3,6 +3,10 @@ package io.poddeck.core.communication;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.poddeck.common.log.Log;
+import io.poddeck.core.communication.agent.command.AgentCommandRegistry;
+import io.poddeck.core.communication.agent.AgentRegistry;
+import io.poddeck.core.communication.handshake.HandshakeService;
+import io.poddeck.core.communication.service.ServiceRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,12 +16,19 @@ import org.springframework.stereotype.Component;
 public final class CommunicationServer {
   private final Log log;
   private final CommunicationConfiguration configuration;
+  private final AgentRegistry agentRegistry;
+  private final AgentCommandRegistry agentCommandRegistry;
+  private final ServiceRepository serviceRepository;
   private Server server;
 
   public void start() {
     try {
       server = ServerBuilder.forPort(configuration.port())
-        .addService(HandshakeService.create()).build().start();
+        .addService(TunnelService.create(log, agentRegistry,
+          agentCommandRegistry, serviceRepository,
+          HandshakeService.create(log, agentRegistry)))
+        .build()
+        .start();
     } catch (Exception exception) {
       log.processError(exception);
     }
