713169a78876b694d72a56f612c6f1c8fdbb0be2
Lukas <contact@lukasbreuer.de>
2025-11-22 19:26:29 +0100

    Update HandshakeService

---
+++ src/main/java/io/poddeck/core/communication/handshake/HandshakeService.java
@@ -0,0 +1,26 @@
+package io.poddeck.core.communication.handshake;
+
+import io.grpc.stub.StreamObserver;
+import io.poddeck.common.HandshakeRequest;
+import io.poddeck.common.TunnelMessage;
+import io.poddeck.common.log.Log;
+import io.poddeck.core.communication.agent.Agent;
+import io.poddeck.core.communication.agent.AgentRegistry;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor(staticName = "create")
+public class HandshakeService {
+  private final Log log;
+  private final AgentRegistry agentRegistry;
+
+  public void process(
+    StreamObserver<TunnelMessage> stream, HandshakeRequest handshakeRequest
+  ) {
+    log.info("HANDSHAKE");
+    //TODO: CHECK CLUSTER ID & KEY
+    var agent = Agent.create(UUID.fromString(handshakeRequest.getCluster()), stream);
+    agentRegistry.register(agent);
+  }
+}
