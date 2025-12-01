382eb9b7871d602bd8e3ec2d43cb17b566b3078f
Lukas <contact@lukasbreuer.de>
2025-12-01 22:54:56 +0100

    Implement DeploymentDeleteController

---
+++ src/main/java/io/poddeck/core/api/panel/deployment/DeploymentDeleteController.java
@@ -0,0 +1,64 @@
+package io.poddeck.core.api.panel.deployment;
+
+import io.poddeck.common.DeploymentDeleteRequest;
+import io.poddeck.common.DeploymentDeleteResponse;
+import io.poddeck.core.api.request.ApiRequestBody;
+import io.poddeck.core.cluster.Cluster;
+import io.poddeck.core.cluster.ClusterRepository;
+import io.poddeck.core.communication.agent.AgentRegistry;
+import io.poddeck.core.communication.agent.command.AgentCommandFactory;
+import io.poddeck.core.member.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Key;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+public final class DeploymentDeleteController extends DeploymentRestController {
+  private final AgentRegistry agentRegistry;
+  private final AgentCommandFactory commandFactory;
+
+  private DeploymentDeleteController(
+    @Qualifier("authenticationKey") Key authenticationKey,
+    MemberRepository memberRepository, ClusterRepository clusterRepository,
+    AgentRegistry agentRegistry, AgentCommandFactory commandFactory
+  ) {
+    super(authenticationKey, memberRepository, clusterRepository);
+    this.agentRegistry = agentRegistry;
+    this.commandFactory = commandFactory;
+  }
+
+  @RequestMapping(path = "/deployment/delete/", method = RequestMethod.POST)
+  public CompletableFuture<Map<String, Object>> deleteDeployment(
+    HttpServletRequest request, @RequestBody String payload,
+    HttpServletResponse response
+  ) {
+    var body = ApiRequestBody.of(payload, response);
+    var namespace = body.getString("namespace");
+    var deployment = body.getString("deployment");
+    return findCluster(request)
+      .thenCompose(cluster -> deleteDeployment(cluster, namespace, deployment));
+  }
+
+  private CompletableFuture<Map<String, Object>> deleteDeployment(
+    Cluster cluster, String namespace, String deployment
+  ) {
+    var agent = agentRegistry.findByCluster(cluster);
+    if (agent.isEmpty()) {
+      return CompletableFuture.completedFuture(Map.of("success", false,
+        "error", 1000));
+    }
+    return commandFactory.create(agent.get())
+      .execute(DeploymentDeleteRequest.newBuilder()
+          .setNamespace(namespace).setDeployment(deployment).build(),
+        DeploymentDeleteResponse.class)
+      .thenApply(response -> Map.of("success", response.getSuccess()));
+  }
+}
