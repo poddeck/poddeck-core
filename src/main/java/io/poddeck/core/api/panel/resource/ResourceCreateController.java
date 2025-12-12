25b8e521bf4dd88efe7177ecf4bda05425beff3a
Lukas <contact@lukasbreuer.de>
2025-12-12 16:33:12 +0100

    Implement ResourceCreateController

---
+++ src/main/java/io/poddeck/core/api/panel/resource/ResourceCreateController.java
@@ -0,0 +1,65 @@
+package io.poddeck.core.api.panel.resource;
+
+import io.poddeck.common.ResourceCreateRequest;
+import io.poddeck.common.ResourceCreateResponse;
+import io.poddeck.core.api.panel.ClusterRestController;
+import io.poddeck.core.api.request.ApiRequestBody;
+import io.poddeck.core.api.security.panel.PanelEndpoint;
+import io.poddeck.core.cluster.Cluster;
+import io.poddeck.core.cluster.ClusterRepository;
+import io.poddeck.core.communication.agent.AgentRegistry;
+import io.poddeck.core.communication.agent.command.AgentCommandFactory;
+import io.poddeck.core.member.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Key;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+public final class ResourceCreateController extends ClusterRestController {
+  private final AgentRegistry agentRegistry;
+  private final AgentCommandFactory commandFactory;
+
+  private ResourceCreateController(
+    @Qualifier("authenticationKey") Key authenticationKey,
+    MemberRepository memberRepository, ClusterRepository clusterRepository,
+    AgentRegistry agentRegistry, AgentCommandFactory commandFactory
+  ) {
+    super(authenticationKey, memberRepository, clusterRepository);
+    this.agentRegistry = agentRegistry;
+    this.commandFactory = commandFactory;
+  }
+
+  @PanelEndpoint
+  @RequestMapping(path = "/resource/create/", method = RequestMethod.POST)
+  public CompletableFuture<Map<String, Object>> createResource(
+    HttpServletRequest request, @RequestBody String payload,
+    HttpServletResponse response
+  ) {
+    var body = ApiRequestBody.of(payload, response);
+    var raw = body.getString("raw");
+    return findCluster(request)
+      .thenCompose(cluster -> createResource(cluster, raw));
+  }
+
+  private CompletableFuture<Map<String, Object>> createResource(
+    Cluster cluster, String raw
+  ) {
+    var agent = agentRegistry.findByCluster(cluster);
+    if (agent.isEmpty()) {
+      return CompletableFuture.completedFuture(Map.of("success", false,
+        "error", 1000));
+    }
+    return commandFactory.create(agent.get())
+      .execute(ResourceCreateRequest.newBuilder().setRaw(raw).build(),
+        ResourceCreateResponse.class)
+      .thenApply(response -> Map.of("success", response.getSuccess()));
+  }
+}
