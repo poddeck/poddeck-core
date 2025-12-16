dac0019e88328778692f8c4f0ca0a023847cf619
Lukas <contact@lukasbreuer.de>
2025-12-16 17:50:54 +0100

    Update SessionRepository

--- src/main/java/io/poddeck/core/session/SessionRepository.java
+++ src/main/java/io/poddeck/core/session/SessionRepository.java
@@ -1,10 +1,16 @@
 package io.poddeck.core.session;
 
 import io.poddeck.core.database.DatabaseRepository;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @Repository
 public interface SessionRepository extends DatabaseRepository<Session, UUID> {
+  @Async
+  CompletableFuture<List<Session>> findSessionsOfMemberByStatus(
+    UUID memberId, SessionStatus status);
 }
