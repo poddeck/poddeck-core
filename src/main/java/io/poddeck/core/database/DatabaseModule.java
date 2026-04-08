805f17991f068fd61592ed560ceabc9665579599
mcz <miguel.cortina.zapp@gmail.com>
2026-04-09 00:05:12 +0200

    Configure HikariCP pool size directly in DatabaseModule

--- src/main/java/io/poddeck/core/database/DatabaseModule.java
+++ src/main/java/io/poddeck/core/database/DatabaseModule.java
@@ -1,8 +1,9 @@
 package io.poddeck.core.database;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import jakarta.persistence.EntityManagerFactory;
 import org.apache.commons.configuration2.AbstractConfiguration;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -23,13 +24,17 @@ public class DatabaseModule {
 
   @Bean
   public DataSource dataSource(DatabaseConfiguration configuration) {
-    return DataSourceBuilder.create()
-      .driverClassName("org.postgresql.Driver")
-      .url(String.format("jdbc:postgresql://%s:%s/%s", configuration.hostname(),
-        configuration.port(), configuration.database()))
-      .username(configuration.username())
-      .password(configuration.password())
-      .build();
+    var hikariConfig = new HikariConfig();
+    hikariConfig.setDriverClassName("org.postgresql.Driver");
+    hikariConfig.setJdbcUrl(String.format("jdbc:postgresql://%s:%s/%s",
+      configuration.hostname(), configuration.port(), configuration.database()));
+    hikariConfig.setUsername(configuration.username());
+    hikariConfig.setPassword(configuration.password());
+    hikariConfig.setMaximumPoolSize(30);
+    hikariConfig.setMinimumIdle(5);
+    hikariConfig.setConnectionTimeout(20000);
+    hikariConfig.setLeakDetectionThreshold(30000);
+    return new HikariDataSource(hikariConfig);
   }
 
   @Bean
