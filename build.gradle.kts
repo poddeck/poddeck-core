1b40f364145d5ae4e27fab215b2b3ae4dcb4bfe0
mcz <miguel.cortina.zapp@gmail.com>
2026-04-27 10:52:37 +0200

    Bump spring-boot to v4.0.6 and drop explicit pring-core:7.0.6 and spring-data-jpa:4.0.4 pins

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import java.util.zip.GZIPInputStream
 
 plugins {
   id("java")
-  id("org.springframework.boot") version "4.0.5"
+  id("org.springframework.boot") version "4.0.6"
   id("io.freefair.lombok") version "9.2.0"
 }
 
@@ -53,10 +53,8 @@ dependencies {
   implementation("com.google.protobuf:protobuf-java:4.34.1")
   implementation("com.google.protobuf:protobuf-java-util:4.34.1")
 
-  implementation("org.springframework.boot:spring-boot-starter-web:4.0.5")
-  implementation("org.springframework:spring-core:7.0.6")
-  implementation("org.springframework.data:spring-data-jpa:4.0.4")
-  implementation("org.springframework.boot:spring-boot-starter-data-jpa:4.0.5")
+  implementation("org.springframework.boot:spring-boot-starter-web:4.0.6")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa:4.0.6")
   implementation("com.h2database:h2:2.4.240")
 
   implementation("de.mkammerer:argon2-jvm:2.12")
